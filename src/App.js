f38475b7970ea550f8b25cf91a94ff5f94bba8e0
AlvaroCordoba-5 <cordobaalvaro16@gmail.com>
2022-12-02 17:41:30 -0300

    whatsapp buttom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { FloatingWhatsApp } from 'react-floating-whatsapp';
 import Video from '../src/videoportada.mp4'
+import Photo from '../src/david.jpg'
 
 function App() {
   
   return (
     <>
-
+<FloatingWhatsApp
+phoneNumber='+5491140575858'
+accountName='David De Los Reyes'
+statusMessage='En linea'
+chatMessage='Hola, como te puedo ayudar?'
+placeholder='Escribe un mensaje...'
+avatar={Photo}
+/>
     <div className='header'>
         <video  autoplay='autoplay' muted loop   id="vid">
          <source type="video/mp4" src={Video} ></source>
@@ -23,7 +32,7 @@ function App() {
  
 
 <div className='ytvideo'>
-
+<h1>Conferencia Corazon disco</h1>
 <iframe  src="https://www.youtube.com/embed/fzcweyZXUgs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div>
 
